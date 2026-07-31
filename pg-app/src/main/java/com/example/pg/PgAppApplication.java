23fa65f99ed10422f0506bc172c10c663324a712
swapnilk30 <sk9137587676@gmail.com>
2026-07-31 15:14:11 +0530

    pg app testing

---
+++ pg-app/src/main/java/com/example/pg/PgAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgAppApplication.class, args);
+	}
+
+}
